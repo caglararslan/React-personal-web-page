fadcd1d6588124c6fca207fadd0caf91a0477483
Çağlar <34140548+caglararslan@users.noreply.github.com>
2023-09-25 22:30:53 +0300

    Update About.jsx

--- caglararslan/src/components/About.jsx
+++ caglararslan/src/components/About.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import myResume from '../assets/files/myresume.pdf';
 
 function About() {
     return (
@@ -13,7 +14,7 @@ function About() {
                     I graduated from Namık Kemal University in 2021 by completing computer engineering.I dealt with many projects during my student years. I started my working life as a front-end developer while still a student. 
                     I am very happy to do the job I love, my biggest goal is to add great value to the projects I take part in by constantly improving myself.
                 </div>
-                <a href='../assets/files/myresume.pdf' className="resume-button" download>Download Resume</a>
+                <a href={myResume} className="resume-button" download>Download Resume</a>
             </Container>
         </div>
     )
